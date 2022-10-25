5cbae25325fe6131ca15e40e9a709eae36dca0fc
shopi <shopin_va@mail.ru>
2022-10-25 20:54:26 +0700

    completed lesson 3:  - created a game, guess a number

---
+++ src/com/geekbrains/lesson3/homework3.java
@@ -0,0 +1,25 @@
+package com.geekbrains.lesson3;
+
+import java.util.Scanner;
+
+public class homework3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число");
+        int range = 100;
+        int number = (int)(Math.random() * range);
+        while (true){
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number){
+                System.out.println("Вы угадали.");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
+        scanner.close();
+    }
+}
