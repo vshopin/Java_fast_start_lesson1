a6e3e2c3116345768b379c147c58837148263e78
shopi <shopin_va@mail.ru>
2022-10-25 22:06:53 +0700

    completed lesson 5:  - created cycle "for"  - changed structure of code

--- src/com/geekbrains/lesson3/homework3.java
+++ src/com/geekbrains/lesson3/homework3.java
@@ -7,13 +7,13 @@ public class homework3 {
 
     public static void main(String[] args) {
         System.out.println("Ваша задача угадать число");
-        int range = 30;
-        int number = (int)(Math.random() * range);
-        playLevel(range, number);
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!");
         scanner.close();
     }
 
-    private static void playLevel(int range, int number){
+    private static void playLevel(int range){
+        int number = (int)(Math.random() * range);
         while (true){
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
